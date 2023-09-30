28ceb382bad9e54c2d87445d327901676e1e24a6
Satyam Shrivastav <mysatyam99@gmail.com>
2023-09-30 20:15:12 +0530

    location decode

--- lib/listcard/temp.dart
+++ lib/listcard/temp.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
 import 'package:acyuta/location/geolocation.dart';
 import 'package:acyuta/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 
 class Temp extends StatefulWidget {
   const Temp({super.key});
@@ -12,10 +12,18 @@ class Temp extends StatefulWidget {
   State<Temp> createState() => _TempState();
 }
 
+late List<Placemark> address;
 Future getWeather() async {
   await getLocation();
   double? latitude = locationData.latitude;
   double? longitude = locationData.longitude;
+  // if (latitude == null || longitude == null) {
+  //   return "Could not get loction";
+  // }
+  address = await placemarkFromCoordinates(latitude!, longitude!,
+      localeIdentifier: "en_IN");
+
+  print(address[0].street);
   final result = await http.get(
     Uri.parse(
       "https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,snowfall_sum,precipitation_hours,windspeed_10m_max&timezone=auto&forecast_days=16",
@@ -41,17 +49,17 @@ class _TempState extends State<Temp> {
                 // widthFactor: 30,
                 // heightFactor: 30,
                 child: CircularProgressIndicator(
-                  strokeAlign: BorderSide.strokeAlignCenter,
-                  semanticsLabel: "Internet Required Loading..",
-                  color: Colors.black,
-                )),
+              strokeAlign: BorderSide.strokeAlignCenter,
+              semanticsValue: "Internet Required Loading..",
+              semanticsLabel: "Internet Required Loading..",
+              color: Colors.black,
+            )),
           );
         }
         final data = snapshot.data;
-        final todayTemp = data['daily']['time'][0] +
-            " : " +
-            data['daily']['temperature_2m_max'][0].toString() +
-            " C";
+        // ignore: prefer_interpolation_to_compose_strings
+        final todayTemp = "${"${address[1].street}" +
+            data['daily']['time'][0]} : ${data['daily']['temperature_2m_max'][0]} C";
         return Card(
           color: const Color.fromARGB(255, 174, 248, 212),
           child: Column(
